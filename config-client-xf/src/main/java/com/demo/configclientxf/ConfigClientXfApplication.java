3a4d87322f64affb5db94f38b4ab87fe3ddeb882
yucw@itsmycar.cn <123456>
2020-01-21 14:09:38 +0800

    添加配置信息

--- config-client-xf/src/main/java/com/demo/configclientxf/ConfigClientXfApplication.java
+++ config-client-xf/src/main/java/com/demo/configclientxf/ConfigClientXfApplication.java
@@ -3,6 +3,9 @@ package com.demo.configclientxf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yiautos
+ */
 @SpringBootApplication
 public class ConfigClientXfApplication {
 
--- config-client-xf/src/main/java/com/demo/configclientxf/controller/TestController.java
+++ config-client-xf/src/main/java/com/demo/configclientxf/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.demo.configclientxf.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author yucw
  * @date 2020/1/21 12:00
  */
+@RefreshScope
 @RestController
 public class TestController {
     @Value("${hello.text}")
